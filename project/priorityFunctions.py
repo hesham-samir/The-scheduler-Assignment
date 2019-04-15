9a99e4ac467b105f975d7e9ae2d1881e5396e1e3
salmaabdelhalim <salmaabdelhalim87@gmail.com>
2019-04-15 20:03:33 +0200

    Create priorityFunctions.py

---
+++ project/priorityFunctions.py
@@ -0,0 +1,98 @@
+class process:
+    def __init__(self, name, arrival, priority, burst):
+        self.name = name
+        self.arrival = arrival
+        self.priority = priority
+        self.burst = burst
+    def stringify(self):
+        return '('+str(self.name)+','+str(self.arrival)+','+str(self.priority)+','+str(self.burst)+')'+"\n"
+
+def csort(p):
+    return p.arrival
+
+class process_list:
+    def __init__(self, list):
+        self.list=list
+
+
+    def insert(self, name, arrival, priority, burst):
+        p = process(name, arrival, priority, burst)
+        self.list.append(p)
+        self.list.sort(key=csort)
+
+    def FCFS(self):
+        wt_time = 0
+        turnaround_time = 0
+        time = self.list[0].arrival
+        for i in range(len(self.list)):
+            time += self.list[i].burst
+            turnaround_time += time - self.list[i].arrival
+            wt_time = wt_time + (turnaround_time - self.list[i].burst)
+
+        print(self)
+        print(wt_time/len(self.list))
+
+    def nonPreemptivePriority(self):
+        wt_time = 0
+        turnaround_time = 0
+        time = self.list[0].arrival
+        for i in range(0, len(self.list)):
+            for j in range(i + 1, len(self.list)):
+                if self.list[j].arrival <= time:
+                    if self.list[j].priority < self.list[i].priority:
+                        self.list[j], self.list[i] = self.list[i], self.list[j]
+            time += self.list[i].burst
+            turnaround_time = time - self.list[i].arrival
+            wt_time = wt_time + (turnaround_time - self.list[i].burst)
+        print(wt_time/len(self.list))
+
+    def preemptivePriority(self):
+        wt_time = 0
+        count = []
+        total_time = self.list[0].arrival
+        for i in range(0, len(self.list)):
+            count.append(0)
+            total_time += self.list[i].burst
+
+        for i in range(self.list[0].arrival, total_time):
+            print(i, " to ", i + 1)
+            flag = 0
+            for j in range(1, len(self.list)):
+
+                if self.list[0].arrival <= i and self.list[j].arrival <= i and self.list[0].burst >= 0 and self.list[j].burst > 0:
+
+                    if self.list[j].priority < self.list[0].priority or self.list[0].burst == 0:
+                        self.list[0], self.list[j] = self.list[j], self.list[0]
+                        count[0], count[j] = count[j], count[0]
+                        count[j] += 1
+
+            if self.list[0].burst > 0:
+                self.list[0].burst -= 1
+            if self.list[0].burst is 0:
+                turnaround_time = (i + 1) - self.list[0].arrival
+                wt_time = wt_time + (turnaround_time - count[0])
+
+            print(self)
+        print(wt_time/len(self.list))
+
+
+    def __str__(self):
+        ls = ""
+        for p in self.list:
+            ls += p.stringify()
+        return ls
+
+'''p1 = process('p1',1,2,3)
+p2 = process('p2',2,3,4)
+plist = [p1, p2]'''
+pro=[]
+pro_queue = process_list(pro)
+pro_queue.insert('p1', 2, 2, 3)
+pro_queue.insert('p2', 4, 5, 6)
+pro_queue.insert('p5', 4, 3, 6)
+pro_queue.insert('p3', 1, 8, 9)
+pro_queue.insert('p4', 10, 11, 12)
+#print(pro_queue)
+#pro_queue.nonPreemptivePriority()
+#pro_queue.preemptivePriority()
+pro_queue.FCFS()
